6416820d25d4272506db725df6c1a5e2dc750ae6
Peeyush Kant Misra <pkmisra1999@gmail.com>
2025-04-02 20:21:27 +0530

    added exception and logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys          # it as a way for your Python script to talk to the operating system and the Python interprete
-
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -19,3 +19,12 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+    
+    
+    
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divideby Zero Error")
+        raise CustomException(e,sys)
