bce4011d831f4debb98deb85b11f901c063f21b0
Peeyush Kant Misra <pkmisra1999@gmail.com>
2025-04-02 20:33:25 +0530

    fixed exception to save in logging file

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys          # it as a way for your Python script to talk to the operating system and the Python interprete
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
