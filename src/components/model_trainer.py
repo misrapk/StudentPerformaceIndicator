9dcc0a5e796d486c80fff9adc219263196e8ce1f
Peeyush Kant Misra <pkmisra1999@gmail.com>
2025-04-30 17:35:16 +0530

    added hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRFRegressor
+from xgboost import XGBRegressor
 
 
 from src.exception import CustomException
@@ -39,13 +39,51 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbor Regressor": KNeighborsRegressor(),
-                "XGBRegressor": XGBRFRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(),
-                "Adaboost Regressor": AdaBoostRegressor()
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
             
-            model_report: dict = evaluate_model(x_train, y_train, x_test, y_test, models)
+            model_report: dict = evaluate_model(x_train, y_train, x_test, y_test, models, params)
             
             best_model_scoer = max(sorted(model_report.values()))
             
@@ -61,7 +99,7 @@ class ModelTrainer:
             
             save_object(
                 file_path = self.model_trainer_config.trained_model_file_path, 
-                obj=best_model
+                obj=best_model  
             )
             
             predicted_output = best_model.predict(x_test)
